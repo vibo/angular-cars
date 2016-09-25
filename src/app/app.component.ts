e1d50e291b410a68ad8a8db977762b8e47f8b8fe
Vibo <vibo92@hotmail.com>
2016-09-25 18:52:02 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+import './app.component.css';
+
+@Component({
+    selector: 'my-app',
+    template: `
+    `
+})
+export class AppComponent { }
